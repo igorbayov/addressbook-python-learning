bc829f661188baad4a722c424c9d864c4e525a83
igorbayov <igorbayov@gmail.com>
2020-12-14 09:44:56 +0300

    Moved the login and logout in the conftest.py

--- conftest.py
+++ conftest.py
@@ -5,5 +5,11 @@ from fixture.application import Application
 @pytest.fixture(scope="session")
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy)
+    fixture.session.login("admin", "secret")
+
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
+
+    request.addfinalizer(fin)
     return fixture
