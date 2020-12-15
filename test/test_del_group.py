2eccec2be4dd40eaae251e2df38f51ee3f553804
igorbayov <igorbayov@gmail.com>
2020-12-15 17:41:36 +0300

    Added preconditions for test_del_group (in case when groups are not)

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,2 +1,7 @@
+from model.group import Group
+
+
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Forced group name", header="Forced header value", footer="Forced footer value"))
     app.group.delete_first_group()
